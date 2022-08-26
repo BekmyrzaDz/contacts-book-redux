68bb0945d1fab3bd5b79f65347792f6085dab520
Bekmyrza <msn.10110922@gmail.com>
2022-08-27 00:33:56 +0600

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import Header from "./components/Header";
 import ContactList from "./components/ContactList/ContactList";
 import ContactItem from "./components/ContactItem/ContactItem";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <ContactList /> */}
-      <ContactItem />
+      <Routes>
+        <Route path="/" element={<ContactList />} />
+        <Route path="/item:id" element={<ContactItem />} />
+      </Routes>
     </div>
   );
 }
